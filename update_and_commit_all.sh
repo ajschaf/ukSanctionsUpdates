4f73a4fd3efe128052d516042b942c1fc7047b32
Aaron Schaffer <40671448+ajschaf@users.noreply.github.com>
2020-09-10 21:00:09 -0400

    Update update_and_commit_all.sh

--- update_and_commit_all.sh
+++ update_and_commit_all.sh
@@ -7,11 +7,11 @@ fetch_and_sort_and_commit () {
     curl -O -J -s --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" "https://ofsistorage.blob.core.windows.net/publishlive/ConList.csv"
     curl -O -J -s --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" "https://ofsistorage.blob.core.windows.net/publishlive/InvBan.csv"
     mv ConList.csv ConList.csv.old
-    mv Invban.csv Invban.csv.old
+    mv InvBan.csv InvBan.csv.old
     echo "Updated" > $commit_txt
     echo $'\n[skip ci]' >> $commit_txt
     git add ConList.csv
-    git add Invban.csv
+    git add InvBan.csv
     git commit -F $commit_txt && \
         git push -q origin master \
         || true
