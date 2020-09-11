43c1968d342dd3870a9d69627fcef3f4ad3dab46
Aaron Schaffer <40671448+ajschaf@users.noreply.github.com>
2020-09-10 20:30:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# newOnFARA
+
+![Fetch latest data, commit](https://github.com/ajschaf/newOnFara/workflows/Fetch%20latest%20data,%20commit/badge.svg)
---
+++ update_and_commit_all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+fetch_and_sort_and_commit () {
+    # $1 is the table name, $2 is the URL
+    local table=$1
+    local csv="$1.csv"
+    local commit_txt="$1.commit.txt"
+    wget --no-check-certificate --progress=dot -O 'ConList.csv' 'https://ofsistorage.blob.core.windows.net/publishlive/ConList.csv'
+    wget --no-check-certificate --progress=dot -O 'InvBan.csv' 'https://ofsistorage.blob.core.windows.net/publishlive/InvBan.csv'
+    mv ConList.csv ConList.csv.old
+    mv InvBan.csv InvBan.csv.old
+    echo "Updated" > $commit_txt
+    echo $'\n[skip ci]' >> $commit_txt
+    git add ConList.csv
+    git add InvBan.csv
+    git commit -F $commit_txt && \
+        git push -q origin master \
+        || true
+}
+
+git config --global user.email "FARAbot@example.com"
+git config --global user.name "FARAbot"
+fetch_and_sort_and_commit
