c624457329f1cb442bb721e8c0a554d0c921112d
Aaron Schaffer <40671448+ajschaf@users.noreply.github.com>
2020-09-10 21:01:51 -0400

    Update update_and_commit_all.sh

--- update_and_commit_all.sh
+++ update_and_commit_all.sh
@@ -4,10 +4,10 @@ fetch_and_sort_and_commit () {
     local table=$1
     local csv="$1.csv"
     local commit_txt="$1.commit.txt"
-    curl -O -J -s --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" "https://ofsistorage.blob.core.windows.net/publishlive/ConList.csv"
-    curl -O -J -s --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" "https://ofsistorage.blob.core.windows.net/publishlive/InvBan.csv"
     mv ConList.csv ConList.csv.old
     mv InvBan.csv InvBan.csv.old
+    curl -O -J -s --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" "https://ofsistorage.blob.core.windows.net/publishlive/ConList.csv"
+    curl -O -J -s --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" "https://ofsistorage.blob.core.windows.net/publishlive/InvBan.csv"
     echo "Updated" > $commit_txt
     echo $'\n[skip ci]' >> $commit_txt
     git add ConList.csv
