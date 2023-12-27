3d3f55bc0d6b9340f4603055db212a6ab07dad36
Aaron Schaffer <40671448+ajschaf@users.noreply.github.com>
2023-12-26 21:24:47 -0500

    Update update_and_commit_all.sh

--- update_and_commit_all.sh
+++ update_and_commit_all.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 fetch_and_sort_and_commit () {
-    # $1 is the table name, $2 is the URL
-    local table=$1
-    local csv="$1.csv"
-    local commit_txt="$1.commit.txt"
-    mv ConList.csv ConList.csv.old
-    mv InvBan.csv InvBan.csv.old
-    curl -O -J -s --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" "https://ofsistorage.blob.core.windows.net/publishlive/ConList.csv"
-    curl -O -J -s --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" "https://ofsistorage.blob.core.windows.net/publishlive/InvBan.csv"
-    echo "Updated" > $commit_txt
-    echo $'\n[skip ci]' >> $commit_txt
+    curl -O -J -s --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" "https://ofsistorage.blob.core.windows.net/publishlive/2022format/ConList.csv" > "ConList.csv"
+    curl -O -J -s --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" "https://ofsistorage.blob.core.windows.net/publishlive/2022format/InvBan.csv" > "InvBan.csv"
     git add ConList.csv
     git add InvBan.csv
-    git commit -F $commit_txt && \
-        git push -q origin master \
-        || true
+    git commit -m "Updated" && \
+    git push -q origin main \
+    || true
 }
 
 git config --global user.email "SanctionsBot"
